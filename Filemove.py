6e1599e8e819af53559c9179fb92f62caf65c2b9
Nirav11 <me.nirav11@gmail.com>
2021-08-25 01:27:24 +0545

    Removed Comment

--- Filemove.py
+++ Filemove.py
@@ -20,28 +20,3 @@ for f in os.listdir("folder"):
 
 
 
-# for f in os.listdir("folder"):
-#     print(os.path.join('folder', f))
-
-#     if not os.path.exists(folderName):
-#         os.mkdir(folderName)
-#         shutil.copy(os.path.join('folder', f), folderName)
-#     else:
-#         shutil.copy(os.path.join('folder', f), folderName)
-
-
-
-### WORKING ##
-# from datetime import datetime 
-
-
-# data = ['2010001', '2010112', '2010365']
-
-# for s in data:
-#     dt = datetime.strptime(s, '%Y%j')
-#     print(dt.monthStamp)
-
-
-### PARTIALLY WORKING ##
-# dt = [datetime.strptime(s, '%Y%j') for s in data]
-# print(dt.monthStamp)
